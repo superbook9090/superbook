b3c10c31ffa7cd4fe106de4e87970e2cba725051
shashank <shashankgupta2215@gmail.com>
2026-08-20 02:19:58 +0530

    blog issue fix

--- src/app/blogs/layout.tsx
+++ src/app/blogs/layout.tsx
@@ -1,14 +1,17 @@
 import MarketingHeader from '@/components/home/MarketingHeader';
 import { LazyHomeFooter } from '@/lib/lazy/home';
+import { QueryProvider } from '@/lib/react-query/QueryProvider';
 
 export default function BlogsLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen flex flex-col">
-      <MarketingHeader forceScrolled={true} />
-      <div className="flex-1 mt-20 sm:mt-24">
-        {children}
+    <QueryProvider>
+      <div className="min-h-screen flex flex-col">
+        <MarketingHeader forceScrolled={true} />
+        <div className="flex-1 mt-20 sm:mt-24">
+          {children}
+        </div>
+        <LazyHomeFooter />
       </div>
-      <LazyHomeFooter />
-    </div>
+    </QueryProvider>
   );
 }
